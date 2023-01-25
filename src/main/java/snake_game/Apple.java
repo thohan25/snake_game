486a77ad26cc15b2066ac9038cf07150adaf9d98
thohan25 <thohan25@bergen.org>
2023-01-25 08:20:07 -0500

    Updated version

---
+++ src/main/java/snake_game/Apple.java
@@ -0,0 +1,14 @@
+package snake_game;
+
+public class Apple {
+
+    private int r;
+    private int c;
+
+    public Apple(int row, int column) {
+        r = row;
+        c = column;
+        
+    }
+    
+}
