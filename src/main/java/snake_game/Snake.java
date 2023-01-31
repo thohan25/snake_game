92085eceb7bb04c89a8283bd647abe4fd974d855
thohan25 <thohan25@bergen.org>
2023-01-30 20:47:57 -0500

    snake framework

--- src/main/java/snake_game/Snake.java
+++ src/main/java/snake_game/Snake.java
@@ -2,8 +2,31 @@ package snake_game;
 
 public class Snake {
 
-    public Snake() {
+    public int[][] indices; // x and y coordinates of each snake body segment, tail comes first
+    public char facing; // N/S/E/W
 
+    public Snake() {
+        indices = new int[][] {{1, 7}, {2, 7}, {3, 7}};
+        facing = 'E';
+    }
+
+    public void checkFacing() {
+        // read arrow key input, do not allow snake to turn into itself
+    }
+
+    public int[][] Move() {
+        for (int i = 0; i < indices.length-1; i++) {
+            indices[i] = indices[i+1]; // moves all segments up by one
+        }
+
+        switch (facing) {
+            case 'N': indices[indices.length-1][1]++;
+            case 'S': indices[indices.length-1][1]--;
+            case 'E': indices[indices.length-1][0]++;
+            case 'W': indices[indices.length-1][0]--;
+        } // moves the head in the direction facing
+
+        return indices;
     }
     
 }
